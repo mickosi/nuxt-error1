1a3ebe53bcc05ea309525054acce492055132dfd
Michael <you@example.com>
2025-08-11 21:41:27 +0300

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-facing-decorator'
+
+@Component()
+class Comp extends Vue {
+    @Prop()
+    prop!: number
+
+    method() { return '' }
+
+    field1 = this.prop //NOT SUPPORT
+    field2 = this.method()//NOT SUPPORT
+}
+</script>
+
+<template>{{ field1 }}</template>
